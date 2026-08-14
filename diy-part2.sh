9ce9585a4d49ed6f3e334c2987ca4fe4eb251cb1
passerby-an <passerby1916@gmail.com>
2026-08-15 00:13:14 +0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=================================================
+# Description: DIY script for ImmortalWrt build
+#=================================================
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# Modify HomeProxy dependency to sing-box-full
+# 1. 修改 HomeProxy 依赖为 sing-box-full（支持 Clash API）
 find package -path "*luci-app-homeproxy/Makefile" -exec sed -i 's/+sing-box/+sing-box-full/g' {} \;
+
+# 2. 生成基础配置
+rm -f .config
+make defconfig
+
+# 3. 添加自定义软件包
+echo "CONFIG_TARGET_mediatek=y" >> .config
+echo "CONFIG_TARGET_mediatek_mt7981=y" >> .config
+echo "CONFIG_TARGET_mediatek_mt7981_DEVICE_h3c_magic-nx30-pro=y" >> .config
+
+echo "CONFIG_PACKAGE_luci=y" >> .config
+echo "CONFIG_PACKAGE_luci-ssl=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-passwall=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+echo "CONFIG_PACKAGE_luci-i18n-homeproxy-zh-cn=y" >> .config
+echo "CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y" >> .config
+echo "CONFIG_PACKAGE_luci-i18n-openclash-zh-cn=y" >> .config
+echo "CONFIG_PACKAGE_sing-box-full=y" >> .config
+echo "CONFIG_PACKAGE_zram-swap=y" >> .config
+
+# 4. 应用配置
+make defconfig
